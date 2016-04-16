324ded7bf6151c84261298c2b0c2b7217f450f0a
Nick Weiland <nickweiland@gmail.com>
2016-04-16 15:52:51 -0600

    add additional feature test for user visiting root

---
+++ spec/features/user_cannot_visit_root_after_login_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "user logs in then visits root" do
+  scenario "user gets redirected to user_path(user)" do
+    VCR.use_cassette("sessions.login") do
+      stub_omniauth
+      visit "/"
+      click_on "signin with twitter"
+
+      expect(current_path).to eq(user_path(ENV['TWITTER_USER_SCREEN_NAME']))
+
+      visit "/"
+
+      expect(current_path).to eq(user_path(ENV['TWITTER_USER_SCREEN_NAME']))
+    end
+ end
+end
