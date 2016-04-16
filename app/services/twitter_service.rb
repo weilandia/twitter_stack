8e8d29c9b0fbe27c2e1bd45b767df9e777b8985a
Nick Weiland <nickweiland@gmail.com>
2016-04-16 11:51:09 -0600

    Resolve naming issues for env vars

--- app/services/twitter_service.rb
+++ app/services/twitter_service.rb
@@ -4,7 +4,7 @@ class TwitterService
   def initialize(user)
     @_client = Twitter::REST::Client.new do |config|
       config.consumer_key    = ENV['TWITTER_CONSUMER_KEY']
-      config.consumer_secret = ENV['TWITTER_CONSUMER_KEY_SECRET']
+      config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
       config.access_token    = user.oauth_token
       config.access_token_secret = user.oauth_token_secret
     end
